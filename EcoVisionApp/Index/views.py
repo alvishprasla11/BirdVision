a02596f6380f1abbcd4b38e9d6e5f5b93943659b
alvishprasla11 <alvish.prasla@gmail.com>
2025-02-15 15:34:52 -0700

    edited views.py by a bit

--- EcoVisionApp/Index/views.py
+++ EcoVisionApp/Index/views.py
@@ -5,5 +5,4 @@ from django.http import HttpResponse
 from django.template import loader
 
 def HomePage(request):
-    template=loader.get_template('index.html')
-    return HttpResponse(template.render())
+    return render(request, 'index.html')
