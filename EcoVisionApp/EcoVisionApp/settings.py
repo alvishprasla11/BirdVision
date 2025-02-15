219753ec162d4982834e6547e02f0f6b60f7f0aa
alvishprasla11 <alvish.prasla@gmail.com>
2025-02-15 14:37:34 -0700

    fix(Django):fixed the django not working error

--- EcoVisionApp/EcoVisionApp/settings.py
+++ EcoVisionApp/EcoVisionApp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("Django-Secret-Key")
+SECRET_KEY = f"{os.getenv("Django-Secret-Key")}"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
