940af484dd20f0480370c1843f74f7ffb19aeffb
alvishprasla11 <alvish.prasla@gmail.com>
2025-02-15 17:44:42 -0700

    fixed issue with manage.py and made it be able to run correctly

--- BirdVisionApp/BirdVisionApp/settings.py
+++ BirdVisionApp/BirdVisionApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = f"{os.getenv("Django-Secret-Key")}"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'BirdVisionApp',
     'Index',
     'chatbot',
     'ImageRecognition',
