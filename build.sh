3b53672fe7a6ea72bb524a3b6828a74c018d6258
alvishprasla11 <alvish.prasla@gmail.com>
2025-02-17 13:29:03 -0700

    trying to make it work

--- build.sh
+++ build.sh
@@ -4,10 +4,13 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-
+# Clear existing staticfiles
+rm -rf BirdVisionApp/staticfiles/*
 # Convert static asset files
 cd BirdVisionApp
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+gunicorn BirdVisionApp.asgi:application --worker-class uvicorn.workers.UvicornWorker --timeout 600
