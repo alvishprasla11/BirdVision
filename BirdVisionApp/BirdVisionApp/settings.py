4794f41c6893eda848a425a47760796afb51a9c8
alvishprasla11 <alvish.prasla@gmail.com>
2025-02-17 13:48:18 -0700

    Final commit before production.

--- BirdVisionApp/BirdVisionApp/settings.py
+++ BirdVisionApp/BirdVisionApp/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = f"{os.getenv("Django-Secret-Key")}"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
